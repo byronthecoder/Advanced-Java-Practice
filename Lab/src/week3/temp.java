7d85dd1547dbfc9320c9e3aa117ed66da7bc8aa4
Flower <hxu31@sheffield.ac.uk>
2018-10-01 23:04:02 +0100

    lab week 3

---
+++ Lab/src/week3/temp.java
@@ -0,0 +1,11 @@
+package week3;
+
+import java.util.Scanner;
+
+public class temp {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(scanner.nextLine().charAt(0));
+        String hhh = "hhh";
+         }
+}
---
+++ Useful tips for JAVA.md
@@ -0,0 +1,15 @@
+# Useful tips for JAVA
+
+- Convert String to int
+
+```java
+String myString = "1234";
+int foo = Integer.parseInt(myString);
+```
+
+```java
+String myString = "1234";
+int foo = Integer.parseInt(myString);
+```
+
+- 
